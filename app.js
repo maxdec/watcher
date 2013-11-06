123e4dde612188cefa8084eb468879e675cadd6e
Maxence <maxence.decrosse@gmail.com>
2013-11-06 11:57:48 +0100

    Init

---
+++ app.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var config = require('config');
+var app = express();
+
+app.configure(function (){
+  app.use(express.bodyParser());
+  app.use(express.static(__dirname + '/public'));
+  app.use(app.router);
+  app.enable('trust proxy');
+});
+
+/**
+ * ROUTES
+ */
+app.get('/', function (req, res){
+  res.send('Everything is fine.');
+});
+
+/**
+ * LET'S GO
+ */
+var server = app.listen(3000);
+console.log('Listening on port 3000');
+
+/**
+ * SOCKET.IO
+ */
+var io = require('socket.io').listen(server);
+io.configure(function () {
+  io.set('log level', 1);
+});
+io.sockets.on('connection', function (socket) {
+  socket.emit('tweets', tweets);
+});
+
+/**
+ * TWITTER
+ */
+var twitter = require('ntwitter');
+var t = new twitter({
+  consumer_key: config.consumer_key,
+  consumer_secret: config.consumer_secret,
+  access_token_key: config.access_token_key,
+  access_token_secret: config.access_token_secret
+});
+
+var search = 'lady gaga,coupure courant,coupure electricite';
+var tweets = [];
+t.stream('statuses/filter', { track: search}, function (stream) {
+  stream.on('data', function (tweet) {
+    tweets = [tweet].concat(tweets).slice(0, 50);
+    io.sockets.emit('tweet', tweet);
+ });
+});
---
+++ public/client.js
@@ -0,0 +1,74 @@
+var socket = io.connect('http://vagrant:3000/');
+
+var tweets = [];
+socket.on('tweets', function (ts) {
+  tweets = ts;
+  drawTweets(tweets);
+});
+socket.on('tweet', function (tweet) {
+  console.log(tweet);
+  tweets = [tweet].concat(tweets).slice(0, 50);
+  drawTweets(tweets);
+});
+
+function drawTweets(tweets) {
+  var html = '';
+  tweets.forEach(function (tweet) {
+    // html += ''+
+    //   '<div class="row">\n'+
+    //     '<div id="tweets" class="col-md-6 col-md-offset-3">\n'+
+    //       '<div class="tweet">\n'+
+    //         '<div class="top">\n'+
+    //           '<a href="//twitter.com/'+tweet.user.screen_name+'/status/'+tweet.id_str+'" class="thumbnail pull-right" target="_blank">\n'+
+    //             '<img src="'+tweet.user.profile_image_url+'">\n'+
+    //           '</a>\n'+
+    //           '<h3>'+tweet.text+'</h3>\n'+
+    //         '</div>\n'+
+    //         '<a href="//twitter.com/'+tweet.user.screen_name+'" class="author" target="_blank">@'+tweet.user.screen_name+'</a>\n'+
+    //         '<a href="//twitter.com/'+tweet.user.screen_name+'/status/'+tweet.id_str+'" class="date" target="_blank">'+timeSince(new Date(tweet.created_at))+'</a>\n'+
+    //       '</div>\n'+
+    //     '</div>\n'+
+    //   '</div>';
+
+    html += ''+
+      '<div class="media tweet">\n'+
+        '<a class="pull-right" href="//twitter.com/'+tweet.user.screen_name+'/status/'+tweet.id_str+'" target="_blank">\n'+
+          '<img class="media-object thumbnail" src="'+tweet.user.profile_image_url+'">\n'+
+        '</a>\n'+
+        '<div class="media-body">\n'+
+          '<h4 class="media-heading">'+tweet.text+'</h4>\n'+
+          '<a href="//twitter.com/'+tweet.user.screen_name+'" class="author" target="_blank">@'+tweet.user.screen_name+'</a>\n'+
+          '<a href="//twitter.com/'+tweet.user.screen_name+'/status/'+tweet.id_str+'" class="date" target="_blank">'+timeSince(new Date(tweet.created_at))+'</a>\n';
+
+    if (tweet.coordinates) {
+      var coords = tweet.coordinates.coordinates;
+      html += '<a href="//maps.google.com/maps?q='+coords[1]+','+coords[0]+'" class="mapmarker" target="_blank"><i class="glyphicon glyphicon-map-marker"></i></a>\n';
+    }
+
+    html += '</div>\n</div>';
+  });
+
+  $('#tweets').html(html);
+}
+
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = Math.floor(seconds / 31536000);
+  if (interval > 1) return date;
+
+  interval = Math.floor(seconds / 2592000);
+  if (interval > 1) return date;
+
+  interval = Math.floor(seconds / 86400);
+  if (interval >= 1) return date;
+
+  interval = Math.floor(seconds / 3600);
+  if (interval >= 1) return interval + ' hours';
+
+  interval = Math.floor(seconds / 60);
+  if (interval >= 1) return interval + ' minutes';
+
+  if (seconds >= 30) return Math.floor(seconds) + ' seconds';
+  return 'now';
+}
